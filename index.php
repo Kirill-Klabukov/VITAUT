51df60e283e955a81ce0331d92396e6f1ae4b3d8
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-03 09:57:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,269 +1,266 @@
-<?php echo '
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>iComputer</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="shortcut icon" href="/img/favicon.ico" type="image/ico">
-    
-    <style>
-    
-    @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500&display=swap');
-
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	outline: none;
-	font-family: Roboto Slab;
-}
-
-a {
-	text-decoration: none;
-	color: inherit;
-}
-
-body {
-	background-color: #ecf0f1;
-}
-
-header {
-	position: sticky;
-	left: 0;
-	top: 0;
-	width: 100%;
-	height: 48px;
-	background-color: #fff;
-	box-shadow: 0 0 4px #dedede;
-}
-
-.delimiter {
-	width: 4px;
-	height: 4px;
-	margin: 10px 6px 6px 6px;
-	background-color: #3d3d3d;
-}
-
-.hor-delimiter {
-	display: block;
-	margin: 12px auto;
-	width: 99%;
-	height: 1px;
-	background-color: #8d8d8d;
-}
-
-.header-container {
-	margin: 0 auto;
-	width: 1200px;
-	height: 100%;
-	display: -webkit-flex;
-	display: -moz-flex;
-	display: -ms-flex;
-	display: -o-flex;
-	display: flex;
-	justify-content: space-between;
-	-ms-align-items: center;
-	align-items: center;
-}
-
-.header-container > .logo {
-	height: 100%;
-	display: -webkit-flex;
-	display: -moz-flex;
-	display: -ms-flex;
-	display: -o-flex;
-	display: flex;
-	-webkit-flex-direction: row;
-	-moz-flex-direction: row;
-	-ms-flex-direction: row;
-	-o-flex-direction: row;
-	flex-direction: row;
-	-ms-align-items: center;
-	align-items: center;
-}
-
-.header-container > .logo > img {
-	height: 80%;
-	margin-right: 8px;
-}
-
-.header-container > .logo > .logo-span {
-	display: -webkit-flex;
-	display: -moz-flex;
-	display: -ms-flex;
-	display: -o-flex;
-	display: flex;
-	-ms-align-items: center;
-	font-weight: 400;
-	font-size: 32px;
-	color: #3d3d3d;
-}
-
-.header-container > .logo > .logo-span > span {
-	font-weight: 400;
-	font-size: 32px;
-	font-weight: 700;
-}
-
-.header-container > .person {
-	height: 100%;
-	display: -webkit-flex;
-	display: -moz-flex;
-	display: -ms-flex;
-	display: -o-flex;
-	display: flex;
-	-ms-align-items: center;
-	align-items: center;
-}
-
-.header-container > .person > a:hover {
-	color: purple;
-	text-decoration: underline;
-}
-
-.search > input[type="text"] {
-	padding: 0 6px;
-	width: 380px;
-	height: 38px;
-	border: 1px solid #3d3d3d;
-	border-right: none;
-	background-color: #fff;
-	font-size: 14px;
-}
-
-.search > input[type="submit"] {
-	text-transform: uppercase;
-	padding: 0 8px;
-	height: 38px;
-	border: 1px solid #3d3d3d;
-	border-left: none;
-	background-color: #fff;
-	font-size: 14px;
-	cursor: pointer;
-}
-.search > input[type="submit"]:hover {
-	background-color: #ecf0f1;
-}
-
-.categories {
-	margin: 8px auto;
-	padding: 8px;
-	width: 1200px;
-	background-color: #fff;
-	box-shadow: 0 0 4px #dedede;
-}
-
-.ci-name {
-	font-size: 16px;
-	color: #5d5d5d;
-}
-
-.ci-blocks {
-	margin: 4px 0;
-}
-
-.ci-block {
-	padding: 2px 4px;
-	margin: 0 1px;
-	border: 1px solid;
-	display: inline-block;
-	font-size: 16px;
-	cursor: pointer;
-	text-transform: uppercase;
-}
-.ci-block:hover {
-	color: #2d98da;
-}
-
-.container {
-	margin: 0 auto;
-	padding: 8px;
-	width: 1200px;
-	height: 200px;
-	background-color: #fff;
-}
-
-.sort-by {
-	display: -webkit-flex;
-	display: -moz-flex;
-	display: -ms-flex;
-	display: -o-flex;
-	display: flex;
-}
-
-.sort-by span {
-	margin-right: 6px;
-}
-
-.sort-by .select {
-	color: #2d98da;
-	cursor: pointer;
-	text-transform: uppercase;
-}
-
-.sort-by .select:hover {
-	text-decoration: underline;
-}    
-    
-    
-    </style>
-</head>
-<body>
-	<header>
-		<div class="header-container">
-			<a class="logo" href="#"><div class="logo-span">&lt;i<span>Computer</span>/&gt;</div></a>
-			<div class="search"><input type="text" placeholder="Enter the product you are looking for"><input type="submit" value="search"></div>
-			<div class="person"><a href="#">Enter</a> <div class="delimiter"></div> <a href="#">Registration</a></div>
-		</div>
-	</header>
-	<div class="categories">
-		<div class="categories-item">
-			<div class="ci-name">Producer:</div>
-			<div class="ci-blocks">
-				<div class="ci-block">Apple</div>
-				<div class="ci-block">Alza.sk</div>
-				<div class="ci-block">Datart.sh</div>
-				<div class="ci-block">Datacomp.sk</div>
-				<div class="ci-block">Alta</div>
-				<div class="ci-block">Avalon</div>
-				<div class="ci-block">Lenovo</div>
-				<div class="ci-block">ACER</div>
-				<div class="ci-block">ASUS</div>
-				<div class="ci-block">Dell</div>
-			</div>
-		</div>
-		<div class="hor-delimiter"></div>
-		<div class="categories-item">
-			<div class="ci-name">Destination:</div>
-			<div class="ci-blocks">
-				<div class="ci-block">Office</div>
-				<div class="ci-block">School</div>
-				<div class="ci-block">Gaming</div>
-			</div>
-		</div>
-		<div class="hor-delimiter"></div>
-		<div class="categories-item">
-			<div class="ci-name">Type:</div>
-			<div class="ci-blocks">
-				<div class="ci-block">PC</div>
-				<div class="ci-block">Notebook</div>
-			</div>
-		</div>
-	</div>
-	<div class="container">
-		<div class="sort-by">
-			<span>Sort:</span>
-			<div class="select">Last:</div>
-		</div>
-		<div class="hor-delimiter"></div>
-		<div class="market-item">
-			
-		</div>
-	</div>
-</body>
-</html>
-
-'; ?>
-
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="en">';
+echo '<head>';
+echo '<meta charset="UTF-8">';
+echo '<title>iComputer</title>';
+echo '<link rel="stylesheet" href="css/style.css">';
+echo '<link rel="shortcut icon" href="/img/favicon.ico" type="image/ico">';
+echo '';
+echo '<style>';
+echo '';
+echo '@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500&display=swap');';
+echo '';
+echo '* {';
+echo 'margin: 0;';
+echo 'padding: 0;';
+echo 'box-sizing: border-box;';
+echo 'outline: none;';
+echo 'font-family: Roboto Slab;';
+echo '}';
+echo '';
+echo 'a {';
+echo 'text-decoration: none;';
+echo 'color: inherit;';
+echo '}';
+echo '';
+echo 'body {';
+echo 'background-color: #ecf0f1;';
+echo '}';
+echo '';
+echo 'header {';
+echo 'position: sticky;';
+echo 'left: 0;';
+echo 'top: 0;';
+echo 'width: 100%;';
+echo 'height: 48px;';
+echo 'background-color: #fff;';
+echo 'box-shadow: 0 0 4px #dedede;';
+echo '}';
+echo '';
+echo '.delimiter {';
+echo 'width: 4px;';
+echo 'height: 4px;';
+echo 'margin: 10px 6px 6px 6px;';
+echo 'background-color: #3d3d3d;';
+echo '}';
+echo '';
+echo '.hor-delimiter {';
+echo 'display: block;';
+echo 'margin: 12px auto;';
+echo 'width: 99%;';
+echo 'height: 1px;';
+echo 'background-color: #8d8d8d;';
+echo '}';
+echo '';
+echo '.header-container {';
+echo 'margin: 0 auto;';
+echo 'width: 1200px;';
+echo 'height: 100%;';
+echo 'display: -webkit-flex;';
+echo 'display: -moz-flex;';
+echo 'display: -ms-flex;';
+echo 'display: -o-flex;';
+echo 'display: flex;';
+echo 'justify-content: space-between;';
+echo '-ms-align-items: center;';
+echo 'align-items: center;';
+echo '}';
+echo '';
+echo '.header-container > .logo {';
+echo 'height: 100%;';
+echo 'display: -webkit-flex;';
+echo 'display: -moz-flex;';
+echo 'display: -ms-flex;';
+echo 'display: -o-flex;';
+echo 'display: flex;';
+echo '-webkit-flex-direction: row;';
+echo '-moz-flex-direction: row;';
+echo '-ms-flex-direction: row;';
+echo '-o-flex-direction: row;';
+echo 'flex-direction: row;';
+echo '-ms-align-items: center;';
+echo 'align-items: center;';
+echo '}';
+echo '';
+echo '.header-container > .logo > img {';
+echo 'height: 80%;';
+echo 'margin-right: 8px;';
+echo '}';
+echo '';
+echo '.header-container > .logo > .logo-span {';
+echo 'display: -webkit-flex;';
+echo 'display: -moz-flex;';
+echo 'display: -ms-flex;';
+echo 'display: -o-flex;';
+echo 'display: flex;';
+echo '-ms-align-items: center;';
+echo 'font-weight: 400;';
+echo 'font-size: 32px;';
+echo 'color: #3d3d3d;';
+echo '}';
+echo '';
+echo '.header-container > .logo > .logo-span > span {';
+echo 'font-weight: 400;';
+echo 'font-size: 32px;';
+echo 'font-weight: 700;';
+echo '}';
+echo '';
+echo '.header-container > .person {';
+echo 'height: 100%;';
+echo 'display: -webkit-flex;';
+echo 'display: -moz-flex;';
+echo 'display: -ms-flex;';
+echo 'display: -o-flex;';
+echo 'display: flex;';
+echo '-ms-align-items: center;';
+echo 'align-items: center;';
+echo '}';
+echo '';
+echo '.header-container > .person > a:hover {';
+echo 'color: purple;';
+echo 'text-decoration: underline;';
+echo '}';
+echo '';
+echo '.search > input[type="text"] {';
+echo 'padding: 0 6px;';
+echo 'width: 380px;';
+echo 'height: 38px;';
+echo 'border: 1px solid #3d3d3d;';
+echo 'border-right: none;';
+echo 'background-color: #fff;';
+echo 'font-size: 14px;';
+echo '}';
+echo '';
+echo '.search > input[type="submit"] {';
+echo 'text-transform: uppercase;';
+echo 'padding: 0 8px;';
+echo 'height: 38px;';
+echo 'border: 1px solid #3d3d3d;';
+echo 'border-left: none;';
+echo 'background-color: #fff;';
+echo 'font-size: 14px;';
+echo 'cursor: pointer;';
+echo '}';
+echo '.search > input[type="submit"]:hover {';
+echo 'background-color: #ecf0f1;';
+echo '}';
+echo '';
+echo '.categories {';
+echo 'margin: 8px auto;';
+echo 'padding: 8px;';
+echo 'width: 1200px;';
+echo 'background-color: #fff;';
+echo 'box-shadow: 0 0 4px #dedede;';
+echo '}';
+echo '';
+echo '.ci-name {';
+echo 'font-size: 16px;';
+echo 'color: #5d5d5d;';
+echo '}';
+echo '';
+echo '.ci-blocks {';
+echo 'margin: 4px 0;';
+echo '}';
+echo '';
+echo '.ci-block {';
+echo 'padding: 2px 4px;';
+echo 'margin: 0 1px;';
+echo 'border: 1px solid;';
+echo 'display: inline-block;';
+echo 'font-size: 16px;';
+echo 'cursor: pointer;';
+echo 'text-transform: uppercase;';
+echo '}';
+echo '.ci-block:hover {';
+echo 'color: #2d98da;';
+echo '}';
+echo '';
+echo '.container {';
+echo 'margin: 0 auto;';
+echo 'padding: 8px;';
+echo 'width: 1200px;';
+echo 'height: 200px;';
+echo 'background-color: #fff;';
+echo '}';
+echo '';
+echo '.sort-by {';
+echo 'display: -webkit-flex;';
+echo 'display: -moz-flex;';
+echo 'display: -ms-flex;';
+echo 'display: -o-flex;';
+echo 'display: flex;';
+echo '}';
+echo '';
+echo '.sort-by span {';
+echo 'margin-right: 6px;';
+echo '}';
+echo '';
+echo '.sort-by .select {';
+echo 'color: #2d98da;';
+echo 'cursor: pointer;';
+echo 'text-transform: uppercase;';
+echo '}';
+echo '';
+echo '.sort-by .select:hover {';
+echo 'text-decoration: underline;';
+echo '}';
+echo '';
+echo '';
+echo '</style>';
+echo '</head>';
+echo '<body>';
+echo '<header>';
+echo '<div class="header-container">';
+echo '<a class="logo" href="#"><div class="logo-span">&lt;i<span>Computer</span>/&gt;</div></a>';
+echo '<div class="search"><input type="text" placeholder="Enter the product you are looking for"><input type="submit" value="search"></div>';
+echo '<div class="person"><a href="#">Enter</a> <div class="delimiter"></div> <a href="#">Registration</a></div>';
+echo '</div>';
+echo '</header>';
+echo '<div class="categories">';
+echo '<div class="categories-item">';
+echo '<div class="ci-name">Producer:</div>';
+echo '<div class="ci-blocks">';
+echo '<div class="ci-block">Apple</div>';
+echo '<div class="ci-block">Alza.sk</div>';
+echo '<div class="ci-block">Datart.sh</div>';
+echo '<div class="ci-block">Datacomp.sk</div>';
+echo '<div class="ci-block">Alta</div>';
+echo '<div class="ci-block">Avalon</div>';
+echo '<div class="ci-block">Lenovo</div>';
+echo '<div class="ci-block">ACER</div>';
+echo '<div class="ci-block">ASUS</div>';
+echo '<div class="ci-block">Dell</div>';
+echo '</div>';
+echo '</div>';
+echo '<div class="hor-delimiter"></div>';
+echo '<div class="categories-item">';
+echo '<div class="ci-name">Destination:</div>';
+echo '<div class="ci-blocks">';
+echo '<div class="ci-block">Office</div>';
+echo '<div class="ci-block">School</div>';
+echo '<div class="ci-block">Gaming</div>';
+echo '</div>';
+echo '</div>';
+echo '<div class="hor-delimiter"></div>';
+echo '<div class="categories-item">';
+echo '<div class="ci-name">Type:</div>';
+echo '<div class="ci-blocks">';
+echo '<div class="ci-block">PC</div>';
+echo '<div class="ci-block">Notebook</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '<div class="container">';
+echo '<div class="sort-by">';
+echo '<span>Sort:</span>';
+echo '<div class="select">Last:</div>';
+echo '</div>';
+echo '<div class="hor-delimiter"></div>';
+echo '<div class="market-item">';
+echo '';
+echo '</div>';
+echo '</div>';
+echo '</body>';
+echo '</html>';
+?>
