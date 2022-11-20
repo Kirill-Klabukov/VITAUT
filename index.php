f9a422d1149d12898ff68d02b245ce89fdef28f5
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 18:27:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,23 @@
 <?php
- 
- 
-/**   
-* The guest book  
-* Гостевая книга   
-* @author IT studio IRBIS-team   
-* @copyright © 2009 IRBIS-team   
-*/   
-/////////////////////////////////////////////////////////  
-/**   
-* We establish the charset and level of errors   
-* Устанавливаем кодировку и уровень ошибок   
-*/   
-header("Content-Type: text/html; charset=utf-8");   
-error_reporting(E_ALL);  
- 
-    $text1 = !empty($_POST['text1'])?$_POST['text1']:null;  
-    $text2 = !empty($_POST['text2'])?$_POST['text2']:null;   
-?>   
-<form action="?" method="post">  
-<input name="text1" type="text" value="<?php echo htmlspecialchars($text1) ?>"/><br />  
-<textarea name="text2" cols="40" rows="10"><?php echo htmlspecialchars($text2); ?></textarea><br />  
-<input name="ok" type="submit" />  
-</form>   
-<?php   
- 
-    if(!empty($_POST['ok']))  
-    {  
-        if(!$text1)  
-            echo 'Текстовое поле не заполнено.';  
-        elseif(!$text2)  
-            echo 'Текстовая область не заполнена.';  
-        else // Записываем информацию в файл, заканчивая  символом переноса строки \n
-            file_put_contents('text.txt', $text2 ."\n", FILE_APPEND);   
-    }  
-    else  
-    {  
-        echo 'Напишите что-нибудь';  
-    }
+        echo '<h1> Index3.php .txt</h1>';
+
+
+        $sn1 = $_GET["a"];
+        $sn2 = $_GET["b"];
+        
+        $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+        $text1 = "a=" . $sn1 . " b=" . $sn2;
+        
+        fwrite($file1, $text1);
+        fclose($file1);
+        
+        $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
+        $text2 = "Value from actuator. Save this value to actuator.txt";
+        fwrite($file2, $text2);
+        fclose($file2);
+        
+        $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
+        $text3 = fread($file3,filesize("actuator.txt"));
+        echo $text3;
+        fclose($file3);
+    ?>
