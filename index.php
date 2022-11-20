f6f56e6c01439c897b2ed59a6834f591cb1b93e6
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 21:07:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Registration</title>
+<title>Registration q</title>
 <meta charset="utf-8" />
 <link rel="shortcut icon" href="favicon.ico" type="image/ico">
     <style>
