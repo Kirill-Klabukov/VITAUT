85b9c83d49d1b7fa98a53abb0bec4d4a5d894896
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 19:21:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <link rel="shortcut icon" href="favicon.ico" type="image/ico">
 </head>
 <body>
-<h2>Form</h2>
+<h2>Form p</h2>
 <form action="input.php" method="POST">
 <p>Login:<br> 
 <input type="text" name="log" /></p>
@@ -17,13 +17,17 @@
 <input type="radio" name="privacy" value="заочно" />Open </p>
 <p>Выберите курсы: <br>
 <select name="courses[]" size="5" multiple="multiple">
-    <option value="ASP.NET">ASP.NET</option>
-    <option value="PHP">PHP</option>
-    <option value="Ruby">RUBY</option>
-    <option value="Python">Python</option>
-    <option value="Java">Java</option>
+    <option value="Game Developer">Game Developer</option>
+    <option value="Software Developer">Software Developer</option>
+    <option value="Software QC Or Quality Control Specialist">Software QC Or Quality Control Specialist</option>
+    <option value="Database Developer">Database Developer</option>
+    <option value="Web Developer">Web Developer</option>
+    <option value="System Hardware Developer">System Hardware Developers</option>
+    <option value="Network System Administrator">Network System Administrator</option>
+    <option value="Analyst Programmer">Analyst Programmer</option>
 </select></p>
-<p> Comment: <br>
+    
+<p> Briefly about myself: <br>
 <textarea name="comment" maxlength="200"></textarea></p>
     
 <p> Agree to the newsletter: <br>
