a5adf7264f4e0779b1b0f9c6a79be444e1dbcf9e
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 21:22:30 +0100

    Update input.php

--- input.php
+++ input.php
@@ -12,47 +12,18 @@ if(isset($_POST["log"]) && isset($_POST["pas"]) && isset($_POST["privacy"]) &&
     $output ="
     <html>
     <head>
-    <title>Data</title>
-    <style>
-         body{
-            background: url(https://images.pexels.com/photos/305821/pexels-photo-305821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1) no-repeat center top fixed;
-            -webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
-        } 
-        
-        .block {
-            
-            width: 200;
-            height: 200;
-            position: absolute;
-            top: 40%;
-            left: 50%;
-            margin: -125px 0 0 -125px;
-            font-family: 'Open Sans', sans-serif;
-            border: 1px solid black; /* Параметры рамки */
-            border-radius: 30px 30px 30px 30px;
-            padding: 15px;
-            background-color: aliceblue;
-            
-            
-        }
-    </style>
-    
+    <title>Data</title>  
     </head>
     <body>
-    <div class = "block">
     Login: $login<br />
     Password: $password<br />
     Privacy: $privacy<br />
     Newseller: $news<br /> 
-    </div>
     Specialization:
     <ul>";
     foreach($spec as $item)
         $output.="<li>" . htmlentities($item) . "</li>";
-    $output.="</ul>  </body></html>";
+    $output.="</ul> </body></html>";
     
     
     
