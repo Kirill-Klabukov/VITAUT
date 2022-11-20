64e24f79f760a949319b99d14e5bd9eb602dd123
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 18:07:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,60 @@
 <meta charset="utf-8" />
 </head>
 <body>
-<?php
-if(isset($_POST["technologies"])){
      
-    $technologies = $_POST["technologies"];
-    foreach($technologies as $item) echo "$item<br />";   
+     <?php
+if(isset($_POST["firstname"]) && isset($_POST["eduform"]) && 
+    isset($_POST["comment"]) && isset($_POST["courses"])) 
+{
+    $name = htmlentities($_POST["firstname"]);
+    $eduform = htmlentities($_POST["eduform"]);
+    $hostel = "нет";
+    if(isset($_POST["hostel"])) $hostel = "да";
+    $comment = htmlentities($_POST["comment"]);
+    $courses = $_POST["courses"];
+    $output ="
+    <html>
+    <head>
+    <title>Анкетные данные</title>
+    </head>
+    <body>
+    Вас зовут: $name<br />
+    Форма обучения: $eduform<br />
+    Требуется общежитие: $hostel<br />
+    Выбранные курсы:
+    <ul>";
+    foreach($courses as $item)
+        $output.="<li>" . htmlentities($item) . "</li>";
+    $output.="</ul></body></html>";
+    echo $output;
+}
+else
+{   
+    echo "Введенные данные некорректны";
 }
 ?>
-<h3>Форма ввода данных</h3>
-<form method="POST">
-    <p>ASP.NET: <input type="checkbox" name="technologies[]" value="ASP.NET" /></p>
-    <p>PHP: <input type="checkbox" name="technologies[]" value="PHP" /></p>
-    <p>Node.js: <input type="checkbox" name="technologies[]" value="Node.js" /></p>
-    <input type="submit" value="Отправить">
+     
+     
+<h2>Анкета</h2>
+<form action="input.php" method="POST">
+<p>Введите имя:<br> 
+<input type="text" name="firstname" /></p>
+<p>Форма обучения: <br> 
+<input type="radio" name="eduform" value="очно" />очно <br>
+<input type="radio" name="eduform" value="заочно" />заочно </p>
+<p>Требуется общежитие:<br>
+<input type="checkbox" name="hostel" />Да</p>
+<p>Выберите курсы: <br>
+<select name="courses[]" size="5" multiple="multiple">
+    <option value="ASP.NET">ASP.NET</option>
+    <option value="PHP">PHP</option>
+    <option value="Ruby">RUBY</option>
+    <option value="Python">Python</option>
+    <option value="Java">Java</option>
+</select></p>
+<p>Краткий комментарий: <br>
+<textarea name="comment" maxlength="200"></textarea></p>
+<input type="submit" value="Отправить">
 </form>
 </body>
 </html>
