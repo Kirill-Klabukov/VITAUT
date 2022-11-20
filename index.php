bd1083376ba3dcb02fb818c2dfc71ac533b6c97c
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 19:44:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
 <meta charset="utf-8" />
 <link rel="shortcut icon" href="favicon.ico" type="image/ico">
     <style>
+        
+        body{
+            background: url(https://images.pexels.com/photos/305821/pexels-photo-305821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1) no-repeat center top fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        } 
+        
+        }
         .main {
             width: 100%;
             height: 100%;
@@ -15,12 +25,60 @@
         }
         
         .block {
-            width: 250px;
-            height: 250px;
+            
+            width: 350;
+            height: 750;
             position: absolute;
             top: 30%;
             left: 50%;
             margin: -125px 0 0 -125px;
+            font-family: 'Open Sans', sans-serif;
+            border: 1px solid black; /* Параметры рамки */
+            border-radius: 30px 30px 30px 30px;
+            padding: 15px;
+            background-color: aliceblue;
+            
+        }
+        
+        .two {
+            background: aliceblue;
+            padding: 50px 20px;
+            text-align: center;
+        } 
+        .two h1 {
+            font-family: 'Open Sans', sans-serif;
+            position: relative;
+            color: #529b49;
+            font-size: 3em;
+            font-weight: normal;
+            line-height: 1;
+            padding: 10px 0;
+            margin: 0;
+            display: inline-block;
+        }
+        .two h1:before {
+            content: ""; 
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            width: 80%;
+            height: 200%;
+            border-radius: 50%/30%;
+            border: 6px solid #529b49;
+            border-left-color: transparent;
+            border-right-color: transparent;
+        }
+        @media (max-width: 420px) {
+            .two h1 {font-size: 2em;}
+        }
+        
+        input {
+            
+           border-radius: 10px 10px 10px 10px; 
+        }
+        
+        
         
 
     </style>    
@@ -29,8 +87,8 @@
 <body>
     <div class = 'main'>
         
-<div class = 'block'>        
-<h2>Registration form</h2>
+<div class = 'block'>  
+<div class="two"><h1>Registration form</h1></div>
 <form action="input.php" method="POST">
 <p>Login:<br> 
 <input type="text" name="log" /></p>
