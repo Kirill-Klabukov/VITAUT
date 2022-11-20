6f8e7f31841e48d59e548c44e055f0ed490c6291
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 16:43:48 +0100

    Update form.php

--- form.php
+++ form.php
@@ -5,8 +5,21 @@
 <meta charset="utf-8" />
 </head>
 <body>
+<?php
+$name = "не определено";
+$age = "не определен";
+if(isset($_POST["name"])){
+  
+    $name = $_POST["name"];
+}
+if(isset($_POST["age"])){
+  
+    $age = $_POST["age"];
+}
+echo "Имя: $name <br> Возраст: $age";
+?>
 <h3>Форма ввода данных</h3>
-<form action="user.php" method="POST">
+<form method="POST">
     <p>Имя: <input type="text" name="name" /></p>
     <p>Возраст: <input type="number" name="age" /></p>
     <input type="submit" value="Отправить">
