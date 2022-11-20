86ea792e77bb6c04d4b1dede77d3e76d3977c376
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 20:30:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Registration </title>
+<title>Registration form</title>
 <meta charset="utf-8" />
 <link rel="shortcut icon" href="favicon.ico" type="image/ico">
     <style>
@@ -13,6 +13,25 @@
             -o-background-size: cover;
             background-size: cover;
         } 
+        
+        .glow-button {
+            
+            text-decoration: none;
+            display: inline-block;
+            padding: 6px 30px;
+            margin: 5px 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 40px 40px #529b49 inset, 0 0 0 0 #529b49;
+            font-family: 'Montserrat', sans-serif;
+            font-weight: bold;
+            letter-spacing: 2px;
+            color: white;
+            transition: .15s ease-in-out;
+        }
+        .glow-button:hover {
+            box-shadow: 0 0 10px 0 #529b49 inset, 0 0 10px 4px #529b49;
+            color: #529b49;
+        }
         
         }
         .main {
@@ -29,7 +48,7 @@
             width: 350;
             height: 750;
             position: absolute;
-            top: 30%;
+            top: 20%;
             left: 50%;
             margin: -125px 0 0 -125px;
             font-family: 'Open Sans', sans-serif;
@@ -37,6 +56,7 @@
             border-radius: 30px 30px 30px 30px;
             padding: 15px;
             background-color: aliceblue;
+            
             
         }
         
@@ -73,10 +93,25 @@
             .two h1 {font-size: 2em;}
         }
         
-        input {
+        .te {
             
-           border-radius: 10px 10px 10px 10px; 
+            border-radius: 10px 10px 10px 10px;
+            
+ 
+            background: #e8e8e8;
+            border: 3px solid #FFF;
+
+  
+        outline: 1px solid #999;
         }
+        
+        .fo {
+         
+            border-radius: 10px 10px 10px 10px;
+            border: 3px solid #FFF;
+            
+        }
+        
         
         
         
@@ -88,17 +123,17 @@
     <div class = 'main'>
         
 <div class = 'block'>  
-<div class="two"><h1>Registration form</h1></div>
+<div class="two"><h1>Registration</h1></div>
 <form action="input.php" method="POST">
 <p>Login:<br> 
-<input type="text" name="log" /></p>
+<input type="text" name="log" class = "te"/></p>
 <p>Password:<br> 
-<input type="password" name="pas" /></p>
-<p>Форма обучения: <br> 
-<input type="radio" name="privacy" value="очно" />Private <br>
-<input type="radio" name="privacy" value="заочно" />Open </p>
-<p>Выберите курсы: <br>
-<select name="courses[]" size="5" multiple="multiple">
+<input type="password" name="pas" class = "te" /></p>
+<p>Profile: <br> 
+<input type="radio" name="privacy" value="Private" />Private <br>
+<input type="radio" name="privacy" value="Open" />Open </p>
+<p>Your specialization: <br>
+<select name="courses[]" size="5" multiple="multiple" class = "fo">
     <option value="Game Developer">Game Developer</option>
     <option value="Software Developer">Software Developer</option>
     <option value="Software QC Or Quality Control Specialist">Software QC Or Quality Control Specialist</option>
@@ -110,12 +145,12 @@
 </select></p>
     
 <p> Briefly about myself: <br>
-<textarea name="comment" maxlength="200"></textarea></p>
+<textarea name="comment" maxlength="200" class = "fo"></textarea></p>
     
 <p> Agree to the newsletter: <br>
 <input type="checkbox" name="news" />yes</p>
     
-<input type="submit" value="Send">
+<input type="submit" value="Send" class="glow-button" >
 </form>
 
 </div>     
