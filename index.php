ac5ef574038b715f8d25edd06309422098495369
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-11-20 18:05:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,22 +6,17 @@
 </head>
 <body>
 <?php
-$name = "не определено";
-$age = "не определен";
-if(isset($_POST["name"])){
-  
-    $name = $_POST["name"];
+if(isset($_POST["technologies"])){
+     
+    $technologies = $_POST["technologies"];
+    foreach($technologies as $item) echo "$item<br />";   
 }
-if(isset($_POST["age"])){
-  
-    $age = $_POST["age"];
-}
-echo "Имя: $name <br> Возраст: $age";
 ?>
 <h3>Форма ввода данных</h3>
 <form method="POST">
-    <p>Имя: <input type="text" name="name" /></p>
-    <p>Возраст: <input type="number" name="age" /></p>
+    <p>ASP.NET: <input type="checkbox" name="technologies[]" value="ASP.NET" /></p>
+    <p>PHP: <input type="checkbox" name="technologies[]" value="PHP" /></p>
+    <p>Node.js: <input type="checkbox" name="technologies[]" value="Node.js" /></p>
     <input type="submit" value="Отправить">
 </form>
 </body>
