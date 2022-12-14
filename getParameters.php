494585f25dc0c406ca7f7414176b035cfba505bf
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-12-14 19:36:54 +0100

    Update getParameters.php

--- getParameters.php
+++ getParameters.php
@@ -3,8 +3,10 @@ echo '<h1>getParameters page</h1>';
 
 $sn1 = $_GET["a"];
 $sn2 = $_GET["b"];
+$sn5 = $_GET["temperature"];
 
-$text = "a=" . $sn1 . " b=" . $sn2;
+
+$text = "a=" . $sn1 . " b=" . $sn2 . "temperature=" . $sn5;
 $sum = $sn1 + $sn2;
 
 echo $text;
