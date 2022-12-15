168358c12223c4c59d973a8c0f285b25a7cfa6a0
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-12-15 14:34:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,28 +11,5 @@ echo '<input type="button" value="ON" onclick="javascript:on();">';
 echo '<input type="button" value="OFF" onclick="javascript:off();">';
 
 
-function on() {
-  // Get the values of the a and b variables
-  var a = document.getElementById("a").value;
-  
-  // Subtract 1 from a
-  var result = a + 1; // ON
-
-  // Display the result on the page
-  document.getElementById("a").innerHTML = result;
-}
-
-
-
-function off() {
-  // Get the values of the a and b variables
-  var a = document.getElementById("a").value;
-  
-  // Subtract 1 from a
-  var result = a - 1; // OFF
-
-  // Display the result on the page
-  document.getElementById("a").innerHTML = result;
-}
 
 ?>
