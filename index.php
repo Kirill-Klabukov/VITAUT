6344a59790964c060485350037359dc12c1535b4
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-12-15 14:40:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,59 @@
 
 $a = 0;
 
-echo '<a id="a">' . $a . '</a>';
+echo '<html> 
+<head>
+  <title> Rele </title>
+  
+  
+  <script>
+  
+  
+function on() {
+  // Get the values of the a and b variables
+  var a = document.getElementById("a").value;
+  
+  // Subtract 1 from a
+  var result = a + 1; // ON
+
+  // Display the result on the page
+  document.getElementById("a").innerHTML = result;
+}
+
+
+
+function off() {
+  // Get the values of the a and b variables
+  var a = document.getElementById("a").value;
+  
+  // Subtract 1 from a
+  var result = a - 1; // OFF
+
+  // Display the result on the page
+  document.getElementById("a").innerHTML = result;
+}
+  
+  
+  
+  </script>
+</head>
+
+<body>
 
 
 
 
-echo '<input type="button" value="ON" onclick="javascript:on();">';
-echo '<input type="button" value="OFF" onclick="javascript:off();">';
+
+<a id="a">' . $a . '</a>
 
 
+
+
+<input type="button" value="ON" onclick="javascript:on();">
+<input type="button" value="OFF" onclick="javascript:off();">
+
+</body>
+
+</html> ';
 
 ?>
