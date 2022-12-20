bba0a9690f06b4a5fd145bdd1ce5d45b84e910db
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-12-20 20:05:56 +0100

    Update file.php

--- file.php
+++ file.php
@@ -4,13 +4,13 @@ $sn1 = $_GET["a"];
 $sn2 = $_GET["b"];
 
 $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
-$text1 = "a=" . $sn1 . " b=" . $sn2;
+$text1 = "TEXT1 a=" . $sn1 . " b=" . $sn2;
 
 fwrite($file1, $text1);
 fclose($file1);
 
 $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
-$text2 = "Value from actuator. Save this value to actuator.txt";
+$text2 = " TEXT2 Value from actuator.  Save this value to actuator.txt";
 fwrite($file2, $text2);
 fclose($file2);
 
