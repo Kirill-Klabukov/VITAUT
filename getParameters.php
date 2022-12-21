82dc40324a6772d2a72e356c1aacc7a9e97c3160
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-12-21 18:33:14 +0100

    Update getParameters.php

--- getParameters.php
+++ getParameters.php
@@ -6,12 +6,24 @@ $sn2 = $_GET["b"];
 $sn5 = $_GET["temperature"];
 $sn6 = $_GET["humidity"];
 
+// Open the text file for writing
+$file = fopen("sensors.txt", "w");
 
-$text = "a=" . $sn1 . " b=" . $sn2 . "temperature=" . $sn5 . "humidity=" . $sn6;
-$sum = $sn1 + $sn2;
+// Write the variables to the file, separated by a comma
+fwrite($file, "$sn1,$sn2,$sn5,$sn6");
 
-echo $text;
-echo "<br>";
-echo "Sum:" . $sum;
+// Close the file
+fclose($file);
+
+
+
+//$text = "a=" . $sn1 . " b=" . $sn2 . "temperature=" . $sn5 . "humidity=" . $sn6;
+//$sum = $sn1 + $sn2;
+
+
+$file3 = fopen("data.txt","r") or die ("Subor neexistuje");
+$text3 = fread($file3,filesize("sensors.txt"));
+echo $text3;
+fclose($file3);
 
 ?>
