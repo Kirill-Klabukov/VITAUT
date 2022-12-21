8b8b7352d010261f07bbd329fecf34e79154c8f2
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-12-21 17:22:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -47,4 +47,18 @@ function off() {
 
 </html> ';
 
+
+$var1 = $a;
+
+
+// Open the text file for writing
+$file = fopen("data.txt", "w");
+
+// Write the variables to the file, separated by a comma
+fwrite($file, "$var1");
+
+// Close the file
+fclose($file);
+
+
 ?>
