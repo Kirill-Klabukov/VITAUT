232cac18bf7d69f97bdeb572ae4517a5af1928fd
Kirill-Klabukov <114649831+Kirill-Klabukov@users.noreply.github.com>
2022-12-22 05:52:36 +0100

    Update dat.php

--- dat.php
+++ dat.php
@@ -2,13 +2,13 @@
 
 // Get the variables from the URL
 $var1 = $_GET['var1'];
-$var2 = $_GET['var2'];
+//$var2 = $_GET['var2'];
 
 // Open the text file for writing
 $file = fopen("data.txt", "w");
 
 // Write the variables to the file, separated by a comma
-fwrite($file, "$var1,$var2");
+fwrite($file, "$var1");
 
 // Close the file
 fclose($file);
